f240846154ea87beef20feee1c7e3ddaca724b82
abel verbo <havelinux@gmail.com>
2018-06-20 17:04:51 +0200

    override controllers

--- src/AppBundle/Controller/EasyAdmin/AdminController.php
+++ src/AppBundle/Controller/EasyAdmin/AdminController.php
@@ -2,25 +2,8 @@
 
 namespace AppBundle\Controller\EasyAdmin;
 
-use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
 
 class AdminController extends BaseAdminController
 {
-  public function changePublishedStatusAction()
-  {
-    $id = $this->request->query->get('id');
-    $entity = $this->em->getRepository('AppBundle:Genus')->find($id);
-
-    $entity->setIsPublished(!$entity->getIsPublished());
-
-    $this->em->flush();
-
-    $this->addFlash('success', sprintf('Genus %spublished!', $entity->getIsPublished() ? '' : 'un'));
-
-    return $this->redirectToRoute('easyadmin', [
-      'action' => 'show',
-      'entity' => $this->request->query->get('entity'),
-      'id' => $id,
-    ]);
-  }
 }
